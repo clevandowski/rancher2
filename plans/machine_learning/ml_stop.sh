7194b155e0001a132b2a236d66e86dc80dd33916
clevandowski <cyrille.levandowski@free.fr>
2020-03-19 08:34:49 +0100

    Suppression fichiers générés par terraform lors de l'arrêt de la VM

--- plans/machine_learning/ml_stop.sh
+++ plans/machine_learning/ml_stop.sh
@@ -6,6 +6,7 @@ source config.sh
 
 destroy_cloud_cluster() {
   terraform destroy -auto-approve -var aws_profile="$AWS_PROFILE" -var aws_region="$AWS_REGION"
+  rm -rf .terraform terraform.tfstate terraform.tfstate.backup
 }
 
 destroy_cloud_cluster
