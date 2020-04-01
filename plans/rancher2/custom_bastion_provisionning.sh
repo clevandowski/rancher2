dad43e4d30637702de06b4fd9ce04b18ea9e24c9
clevandowski <cyrille.levandowski@free.fr>
2020-04-01 09:29:52 +0200

    Traefik utilise maintenant un NLB, et on crée un alias en wildcard (ex: *.cyrille.aws.zenika.com) sur le DNS externe de ce NLB

--- plans/rancher2/custom_bastion_provisionning.sh
+++ plans/rancher2/custom_bastion_provisionning.sh
@@ -1,12 +1,13 @@
 # Parce que je sais que je détruis le bastion à la fin
 # Ne pas faire ça si le bastion reste après usage
 # Ne jamais partager un bastion avec ça
-scp -o StrictHostKeyChecking=no -F ssh_config -r ~/.aws rancher2-bastion:~
-scp -o StrictHostKeyChecking=no -F ssh_config -r ./elasticsearch rancher2-bastion:~
-scp -o StrictHostKeyChecking=no -F ssh_config -r ./traefik rancher2-bastion:~
-ssh -o StrictHostKeyChecking=no -F ssh_config rancher2-bastion 'echo "export AWS_PROFILE=clevandowski-ops-zenika" >> .bashrc'
-ssh -o StrictHostKeyChecking=no -F ssh_config rancher2-bastion 'echo "export AWS_REGION=eu-north-1" >> .bashrc'
-ssh -o StrictHostKeyChecking=no -F ssh_config rancher2-bastion 'sudo yum install git -y'
-# kubectl -n cattle-system exec $(kubectl -n cattle-system get pods -l app=rancher | grep '1/1' | head -1 | awk '{ print $1 }') -- reset-password | tail -n 1 > rancher_admin_password.txt
-# ssh -F ssh_config rancher2-bastion kubectl -n cattle-system exec \$(kubectl -n cattle-system get pods -l app=rancher | grep "1/1" | head -1 | awk "{ print \$1 }") -- reset-password | tail -n 1 > rancher_admin_password.txt
-# ssh -F ssh_config rancher2-bastion
+scp -F ssh_config -r ~/.aws rancher2-bastion:~
+scp -F ssh_config -r ./elasticsearch rancher2-bastion:~
+scp -F ssh_config -r ./traefik rancher2-bastion:~
+ssh -F ssh_config rancher2-bastion 'echo "export AWS_PROFILE=clevandowski-ops-zenika" >> .bashrc'
+ssh -F ssh_config rancher2-bastion 'echo "export AWS_REGION=eu-north-1" >> .bashrc'
+ssh -F ssh_config rancher2-bastion 'sudo yum install -y git jq'
+ssh -F ssh_config rancher2-bastion "~/.local/bin/kubectl -n cattle-system exec \$(~/.local/bin/kubectl -n cattle-system get pods -l app=rancher | grep '1/1' | head -1 | awk '{ print \$1 }') -- reset-password | tail -n 1 > rancher_admin_password.txt"
+ssh -F ssh_config rancher2-bastion 'cat rancher_admin_password.txt'
+echo "Connect to bastion:"
+echo "  ssh -F ssh_config rancher2-bastion"
--- plans/rancher2/traefik/traefik_start.sh
+++ plans/rancher2/traefik/traefik_start.sh
@@ -1,5 +1,56 @@
 #!/bin/bash
 
+set -eo pipefail
+
+export ROOT_DNS="aws.zenika.com"
+export TRAEFIK_LB_DNS="*.cyrille.aws.zenika.com"
+export HOSTED_ZONE_ID=$(aws route53 list-hosted-zones-by-name | jq -r ".HostedZones[] | select(.Name==\"${ROOT_DNS}.\") | .Id" | sed -e 's|^.*/\([^/]*\)$|\1|')
+
+getLoadBalancerDNS() {
+  # Comme je ne sais pas encore donner un petit nom au load balancer généré par traefik
+  # Je choppe le dernier qui a été créé
+  aws --region $AWS_REGION elbv2 describe-load-balancers \
+  | jq -r '.LoadBalancers | sort_by(.CreatedTime) | last | .DNSName'
+}
+getLoadBalancerHostedZoneId() {
+  # Comme je ne sais pas encore donner un petit nom au load balancer généré par traefik
+  # Je choppe le dernier qui a été créé
+  aws --region $AWS_REGION elbv2 describe-load-balancers \
+  | jq -r '.LoadBalancers | sort_by(.CreatedTime) | last | .CanonicalHostedZoneId'
+}
+
+getRecordSetConfig() {
+  local hostedZoneId="$1"
+  local publicTraefikLBDNS="$2"
+  local internalTraefikLBDNS="$3"
+  local internalTraefifLBHostedZoneId="$4"
+  cat << COINCOIN
+{
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "${publicTraefikLBDNS}",
+      "Type": "A",
+      "AliasTarget": {
+        "HostedZoneId": "${internalTraefifLBHostedZoneId}",
+        "DNSName": "${internalTraefikLBDNS}",
+        "EvaluateTargetHealth": true
+      }
+    }}]
+}
+COINCOIN
+}
+
+createAliasRecordSet() {
+  local hostedZoneId="$1"
+  local publicTraefikLBDNS="$2"
+  local internalTraefikLBDNS="$(getLoadBalancerDNS)"
+  local internalTraefifLBHostedZoneId="$(getLoadBalancerHostedZoneId)"
+  local recordSetConfig="$(getRecordSetConfig "$hostedZoneId" "$publicTraefikLBDNS" "$internalTraefikLBDNS" "$internalTraefifLBHostedZoneId")"
+  # echo "recordSetConfig: $recordSetConfig"
+  aws route53 change-resource-record-sets --hosted-zone-id "$hostedZoneId" --change-batch "$recordSetConfig"
+}
+
 if ! kubectl get clusterrolebinding traefik-ingress-controller 2>/dev/null; then
   echo "Traefik cluster role and cluster role binding not defined"
   kubectl apply -f traefik-rbac.yaml
@@ -20,6 +71,11 @@ else
     --set acme.dnsProvider.route53.AWS_SECRET_ACCESS_KEY=$(cat ~/.aws/credentials | grep ${AWS_PROFILE} -A 2 | grep aws_secret_access_key | awk '{ print $3 }') \
     --set acme.dnsProvider.route53.AWS_REGION=${AWS_REGION}
 fi
+
+kubectl -n default rollout status deploy/traefik
+
+echo "Creating DNS Recordset $TRAEFIK_LB_DNS on internal traefik DNS"
+createAliasRecordSet "$HOSTED_ZONE_ID" "$TRAEFIK_LB_DNS"
 
 if ! kubectl get service kibana-kibana 2>/dev/null; then
   kubectl apply -f kibana-ingress.yml
